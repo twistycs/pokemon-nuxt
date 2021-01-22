e9813b93ea4d7f97258eda1b63ac2a848f08b08e
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-22 18:04:25 +0700

    add and remove button

--- pages/pokemon.vue
+++ pages/pokemon.vue
@@ -2,6 +2,9 @@
   <div>
   <h2>Welcome to Pokemon Master</h2>
   <!-- <h2>{{pokemonList}}</h2> -->
+  <h2>{{addPokemonList}}</h2>
+  <h2>{{removePokemonList}}</h2>
+  <a :href="/pokemon_trainer/+username">Back</a>
 <br>
   <div  v-for="(pokemon, index) in pokemonList" :key="index">
     <b-card :img-src="pokemon.ImageUrl" img-alt="Card image" img-left class="mb-3">
@@ -11,16 +14,19 @@
         <h2>Super-Type : {{pokemon.SuperType}}</h2>
         <h2>Sub-Type : {{pokemon.SubType}}</h2>
         <h2>Type : {{pokemon.Type}}</h2>
+        <br>
+        <b-button @click="addPokemon(pokemon.ID,pokemon.Name)" variant="success">Add Pokemon</b-button>
+        <b-button @click="removePokemon(pokemon.ID,pokemon.Name)" variant="danger">Remove Pokemon</b-button>
       </b-card-text>
     </b-card>
   </div>
   </div>
- 
 </template>
 
 <script>
 import axios from 'axios';
 export default {
+  middleware : 'auth',
   mounted() {
           const cookieToken = this.$cookies.get('token')
           axios({
@@ -40,8 +46,19 @@ export default {
     },
     data(){
         return {
-            pokemonList : []
+            username : this.$cookies.get('username'),
+            pokemonList : [],
+            addPokemonList : [],
+            removePokemonList : []
         }
+    },
+    methods:{
+      addPokemon(id,name){
+        this.addPokemonList.push(name)
+      },
+      removePokemon(id,name){
+        this.removePokemonList.push(name)
+      }
     }
 }
 </script>
